b24e53685b5acee6da15ad390dd0177e436e6620
Artur Gubaidullin <lapteuh@gmail.com>
2023-04-16 16:57:50 +0400

    Add SimpleContract

--- packages/data-transfer-contract/src/lib/data-transfer-contract.ts
+++ packages/data-transfer-contract/src/lib/data-transfer-contract.ts
@@ -1,3 +1,22 @@
-export function dataTransferContract(): string {
-  return 'data-transfer-contract';
-}
+import * as t from 'io-ts';
+
+const RequestMessage = t.readonly(
+  t.exact(
+    t.type({
+      content: t.string,
+    })
+  )
+);
+
+const ResponseMessage = t.readonly(
+  t.exact(
+    t.type({
+      result: t.string,
+    })
+  )
+);
+
+export const SimpleContract = Object.freeze({
+  request: RequestMessage,
+  response: ResponseMessage,
+});
